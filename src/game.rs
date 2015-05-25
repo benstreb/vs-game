0c373d48580f538e544c9393fffe75528b923797
Benjamin Streb <Bstreb@gmail.com>
2015-05-25 15:13:16 -0700

    More progress towards v0.1
    Renamed Tile to TileColor
    Added new Tile struct to represent a specific tile
    Added texture function to get the texture for a specific color
      It feels wrong somehow, like I should have a registry so the
      refcounting has a purpose. This will do for now, though
    Added Tile::new

--- src/game.rs
+++ src/game.rs
@@ -1,16 +1,47 @@
+use std::path::Path;
+use std::rc::Rc;
+use uuid::Uuid;
 use opengl_graphics::{GlGraphics, Texture};
-use piston::event::RenderArgs;
-use sprite::Scene;
+use piston::event::Event;
+use sprite::{Sprite, Scene};
 
 trait Game {
     fn event(&self, gl: &mut GlGraphics, event: &Event);
 }
 
-enum Tile {
+enum TileColor {
     RED,
     GREEN,
     BLUE,
     YELLOW,
+}
+
+impl TileColor {
+    fn texture(&self) -> Rc<Texture> {
+        Rc::new(Texture::from_path(Path::new(match self {
+            &TileColor::RED => "./bin/assets/red_box.png",
+            &TileColor::GREEN => "./bin/assets/red_box.png",
+            &TileColor::BLUE => "./bin/assets/red_box.png",
+            &TileColor::YELLOW => "./bin/assets/red_box.png",
+        })).unwrap())
+    }
+}
+
+struct Tile {
+    color: TileColor,
+    sprite_id: Uuid,
+}
+
+impl Tile {
+    fn new(color: TileColor, scene: &mut Scene<Texture>) -> Tile {
+        let mut sprite = Sprite::from_texture(color.texture());
+        let sprite_id = sprite.id();
+        scene.add_child(sprite);
+        Tile {
+            color: color,
+            sprite_id: sprite_id,
+        }
+    }
 }
 
 struct UnnamedGame {
